5acb0d16d69e1f3520ffc95e40e9e3c4e12dfe89
nagireddy panditi <nagireddy.panditi@gmail.com>
2020-03-24 18:49:45 +0530

    changes added

---
+++ src/Components/Projects/ReactProjects.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Row, Col,Card,Button} from "react-bootstrap";
+
+const ReactProjects = () => {
+  return (
+    <div className="container single-project" style={{ width: '70%', margin: "0 auto" }}>
+      <div className="row text-center">
+        <div className="col-md-6">
+            <h1>Chingu Store </h1>
+            <p>
+            Application fetches clothes data from ASOS.com API. User can search by name, sort, load more items and add them to the basket. Chingu Store was created in the team of 3 people. I learned a lot about Git workflow and project management. ⚠ Live demo currently offline
+           </p>
+           <button>button-1</button>
+           <button>button-2</button>
+        </div>        
+        <div className="col-md-6">
+            <img src="https://cdn.eso.org/images/thumb300y/eso1907a.jpg" alt="nag" height="200px" width="300px" />
+        </div>        
+        <div className="col-md-6">
+            <h1>Chingu Store </h1>
+            <p>
+            Application fetches clothes data from ASOS.com API. User can search by name, sort, load more items and add them to the basket. Chingu Store was created in the team of 3 people. I learned a lot about Git workflow and project management. ⚠ Live demo currently offline
+           </p>
+           <button>button-1</button>
+           <button>button-2</button>
+        </div>        
+        <div className="col-md-6">
+            <img src="https://cdn.eso.org/images/thumb300y/eso1907a.jpg" alt="nag" height="200px" width="300px" />
+        </div>        
+        <div className="col-md-6">
+            <h1>Chingu Store </h1>
+            <p>
+            Application fetches clothes data from ASOS.com API. User can search by name, sort, load more items and add them to the basket. Chingu Store was created in the team of 3 people. I learned a lot about Git workflow and project management. ⚠ Live demo currently offline
+           </p>
+           <button>button-1</button>
+           <button>button-2</button>
+        </div>        
+        <div className="col-md-6">
+            <img src="https://cdn.eso.org/images/thumb300y/eso1907a.jpg" alt="nag" height="200px" width="300px" />
+        </div>        
+       </div>
+    </div>
+
+  )
+
+
+}
+
+export default ReactProjects;
--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,36 +1,28 @@
-import React, { Component } from "react";
-import { Container, Row, Col,Tabs,Tab } from 'react-bootstrap';
+import React, { useState } from "react";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import ReactProjects from './Projects/ReactProjects';
 
-export default class Projects extends React.Component {
-    state = {
-
-    }
-
-    render() {
-        return (
-            <div className="project-container container">
-            <div className="main-name-container">
-                <Container>
-                    <Row>
-                        <Col style={{display:"flex", justifyContent:"center"}}>
-
-                            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" variant="pills">
-                                <Tab eventKey="home" title="React Js">
-                                    home
-                                </Tab>
-                                <Tab eventKey="profile" title="HTML">
-                                    profile
-                                </Tab>
-                                <Tab eventKey="contact" title="MongoDB" >
-                                    contact
-                                </Tab>
-                            </Tabs>
-
-                        </Col>
-                    </Row>
-                </Container>
+const Projects = () => {
+    
+    return (
+        <div className="project-container container">
+            <div className="main-project-container">
+                <Tabs defaultActiveKey="reactJs" className="justify-content-center" variant="pills">
+                    <Tab eventKey="reactJs" title="React JS">
+                        <ReactProjects />
+                    </Tab>
+                    <Tab eventKey="html" title="HTML">
+                        <h1>Profile</h1>
+                    </Tab>
+                    <Tab eventKey="bootstrap" title="Bootstrap" >
+                    <h1>Contact</h1>
+                    </Tab>
+                </Tabs>
             </div>
-            </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default Projects;
+
