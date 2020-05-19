f16494e6ce2aaecf7a2f8975ee862fcccc6a240a
nagireddy panditi <nagireddy.panditi@gmail.com>
2020-05-20 00:20:44 +0530

    changes adde

--- src/Components/Assests/nodeprojectdetails.js
+++ src/Components/Assests/nodeprojectdetails.js
@@ -1,5 +1,4 @@
 import img1 from '../../images/waves-project.JPG';
-import img2 from '../../images/movies-app.JPG';
 
 export default [
 
@@ -14,16 +13,4 @@ export default [
     "image":img1,
     "image_describe":"Waves-Project"
   },
-  
-  {
-    "id": 2,
-    "name": "Movies-App",
-    "description": "This is a React app with Redux where users can find any movie of their choice and get information about that movie. The data is taken from an API which reflects IMDB movie data. This app can be enhanced as a cinema website where people can buy their tickets for the movies.",
-    "code_link": "https://github.com/nagireddy1234/E-commerce_capstoneProject",
-    "deploy_link": "https://ecommerce-mobilestore.netlify.com/",
-    "tools": ["React Js", "Redux","local storage" ],
-    "icons": ["fab", "react"],
-    "image":img2,
-    "image_describe":"colors-project"
-  }, 
 ]
