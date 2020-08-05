24fb02c5e29483b70bc4646b912660113b03adef
nagiReddy <nagireddy.panditi@gmail.com>
2020-08-05 13:08:15 +0530

    changes added

--- src/Components/Assests/Phpprojectdetails.js
+++ src/Components/Assests/Phpprojectdetails.js
@@ -1,28 +1,30 @@
-import img1 from '../../images/bookstorephp.JPG';
-import img2 from '../../images/coffeeshop.JPG';
+import img1 from '../../images/coffeeshop.JPG';
+import img2 from '../../images/bookstorephp.JPG';
 
 export default [
 
   {
     "id": 1,
+    "name": "Nagi's Coffee-shop",
+    "description": "This is a coffeshop app using PHP. I have done login and registration aithentication.sending messages via php mailer",
+    "code_link": "https://github.com/nagireddy1234/coffeeShop-php/",
+    "deploy_link": "https://shielded-thicket-36277.herokuapp.com",
+    "tools": ["PHP", "Jquery","Bootstrap","CSS" ],
+    "icons": ["fab", "PHP"],
+    "image":img1,
+    "image_describe":"Waves-Project"
+  },
+  
+  {
+    "id": 2,
     "name": "Book-Store",
     "description": "This is Book store app and PHP CRUD operation are done",
     "code_link": "https://github.com/nagireddy1234/BookStorePHP",
     "deploy_link": "https://github.com/nagireddy1234/BookStorePHP",
     "tools": ["PHP", "Javascript","CSS" ],
     "icons": ["fab", "PHP"],
-    "image":img1,
-    "image_describe":"Waves-Project"
-  },
-  {
-    "id": 2,
-    "name": "Nagi's Coffee-shop",
-    "description": "This is a coffeshop app using PHP. I have done login and registration aithentication.sending messages via php mailer",
-    "code_link": "https://shielded-thicket-36277.herokuapp.com/",
-    "deploy_link": "https://github.com/nagireddy1234/coffeeShop-php",
-    "tools": ["PHP", "Jquery","Bootstrap","CSS" ],
-    "icons": ["fab", "PHP"],
     "image":img2,
     "image_describe":"Waves-Project"
   },
+  
 ]
