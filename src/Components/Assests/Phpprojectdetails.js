d5c27d7b5ef07a875e61c3068c8ea4ba6a956ae0
nagiReddy <nagireddy.panditi@gmail.com>
2020-08-17 22:15:07 +0530

    added ecommerce project to php

--- src/Components/Assests/Phpprojectdetails.js
+++ src/Components/Assests/Phpprojectdetails.js
@@ -1,5 +1,6 @@
 import img1 from '../../images/coffeeshop.JPG';
-import img2 from '../../images/bookstorephp.JPG';
+import img2 from '../../images/mobile-php.JPG';
+import img3 from '../../images/bookstorephp.JPG';
 
 export default [
 
@@ -14,17 +15,28 @@ export default [
     "image":img1,
     "image_describe":"Waves-Project"
   },
-  
   {
     "id": 2,
+    "name": "Mobie-store",
+    "description": "This is mobile shop app using PHP ",
+    "code_link": "https://github.com/nagireddy1234/E-commerce-php",
+    "deploy_link": "https://github.com/nagireddy1234/E-commerce-php",
+    "tools": ["PHP", "Jquery","Bootstrap","CSS" ],
+    "icons": ["fab", "PHP"],
+    "image":img2,
+    "image_describe":"mobile-store-php"
+  },
+  
+  {
+    "id": 3,
     "name": "Book-Store",
     "description": "This is Book store app and PHP CRUD operation are done",
     "code_link": "https://github.com/nagireddy1234/BookStorePHP",
     "deploy_link": "https://github.com/nagireddy1234/BookStorePHP",
     "tools": ["PHP", "Javascript","CSS" ],
     "icons": ["fab", "PHP"],
-    "image":img2,
-    "image_describe":"Waves-Project"
+    "image":img3,
+    "image_describe":"book-store-php"
   },
   
 ]
