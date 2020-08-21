bbed7bb432acfd42522640842d1ffd3ab24ac5e9
nagiReddy <nagireddy.panditi@gmail.com>
2020-08-21 23:59:36 +0530

    changes added

--- src/Components/Pages/projects.js
+++ src/Components/Pages/projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
         <div>
         <div className="project-container container">
             <div className="main-project-container" style={{backgroundColor:"#fff"}}>
-                <Tabs defaultActiveKey="html" className="justify-content-center" variant="pills">
+                <Tabs defaultActiveKey="reactJs" className="justify-content-center" variant="pills">
                     <Tab eventKey="reactJs" title="React JS">
                         <ReactProjects />
                     </Tab>
