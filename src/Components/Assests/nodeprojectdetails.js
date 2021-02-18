110e37b49b8bb616d711e7a7bd114f5da9066cd3
nagiReddy <nagireddy.panditi@gmail.com>
2021-02-18 10:38:57 +0200

    Update nodeprojectdetails.js

--- src/Components/Assests/nodeprojectdetails.js
+++ src/Components/Assests/nodeprojectdetails.js
@@ -7,7 +7,7 @@ export default [
     "name": "Waves-Project",
     "description": "This is a React app with Redux where users can find any movie of their choice and get information about that movie. The data is taken from an API which reflects IMDB movie data. This app can be enhanced as a cinema website where people can buy their tickets for the movies.",
     "code_link": "https://github.com/nagireddy1234/E-commerce_capstoneProject",
-    "deploy_link": "https://ecommerce-mobilestore.netlify.com/",
+    "deploy_link": "https://github.com/nagireddy1234/E-commerce_capstoneProject",
     "tools": ["React Js", "Redux","local storage" ],
     "icons": ["fab", "react"],
     "image":img1,
