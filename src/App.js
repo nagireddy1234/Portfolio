60b40fa56177c63190314d4c8cdbe4235b48c9c8
nagireddy1234 <nagireddy.panditi@gmail.com>
2021-12-05 09:04:19 +0200

    Changes added

--- src/App.js
+++ src/App.js
@@ -1,83 +1,116 @@
-import React, { Component } from 'react';
-import './App.css';
-import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
-import Navbar from './Components/Pages/Navbar';
-import {Link} from 'react-router-dom';
-import ParticlesContainer from './Components/Reusable/ParticlesContainer';
-import {BrowserRouter} from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Navbar from "./Components/Pages/Navbar";
+import { Link } from "react-router-dom";
+import ParticlesContainer from "./Components/Reusable/ParticlesContainer";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-
-    togglechange = ()=> {
-        let closeDrawer = document.querySelector(".mdl-layout__drawer");
-        let closeDrawerButton =document.querySelector(".mdl-layout__drawer-button");
-        let closeObfuscator= document.querySelector(".mdl-layout__obfuscator")
-        closeDrawer.classList.remove("is-visible");
-        closeObfuscator.classList.remove("is-visible")
-        closeDrawer.removeAttribute('aria-hidden')
-        closeDrawer.setAttribute("aria-hidden",false)
-        closeDrawerButton.setAttribute("aria-hidden",true)
+  togglechange = () => {
+    let closeDrawer = document.querySelector(".mdl-layout__drawer");
+    let closeDrawerButton = document.querySelector(
+      ".mdl-layout__drawer-button"
+    );
+    let closeObfuscator = document.querySelector(".mdl-layout__obfuscator");
+    closeDrawer.classList.remove("is-visible");
+    closeObfuscator.classList.remove("is-visible");
+    closeDrawer.removeAttribute("aria-hidden");
+    closeDrawer.setAttribute("aria-hidden", false);
+    closeDrawerButton.setAttribute("aria-hidden", true);
+  };
+  addActiveClass = () => {
+    var links = document.getElementsByClassName("mdl-navigation__link");
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+      });
     }
-    addActiveClass = () => {
-        var links =document.getElementsByClassName('mdl-navigation__link');
-        for (var i = 0; i < links.length; i++) {
-            links[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName("active");
-            if (current.length > 0) { 
-              current[0].className = current[0].className.replace(" active", "");
-            }
-            this.className += " active";
-            });
-  }
-    }
-  render()
-   {
+  };
+  render() {
     return (
-        <BrowserRouter>
-<div className="demo-big-content" style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100vh",
-          overflow: "hidden",
-        }}>   
-    <ParticlesContainer  />
-    <div
+      <BrowserRouter>
+        <div
+          className="demo-big-content"
           style={{
             position: "absolute",
             top: 0,
             left: 0,
             width: "100%",
-            height: "100%"
+            height: "100vh",
+            overflow: "hidden",
           }}
         >
-    <Layout>
-        <Header className= 'header-color' title={<Link className="header-link" to="/"> Nagi Reddy</Link>} scroll>
-            <Navigation>
-                <Link to="/about" onClick={this.addActiveClass}>About</Link>
-                <Link to="/projects" onClick={this.addActiveClass}>Projects</Link>
-                <Link to="/cv" onClick={this.addActiveClass}>CV</Link>
-                <Link to="/contact" onClick={this.addActiveClass}>Contact</Link>
-            </Navigation>
-        </Header> 
-        <Drawer title={<Link className="drawer-link" to="/">Nagi Reddy</Link>}>
-            <Navigation>
-                <Link to="/about" onClick={this.togglechange} >About</Link>
-                <Link to="/projects" onClick={this.togglechange}>Projects</Link>
-                <Link to="/cv" onClick={this.togglechange}>CV</Link>
-                <Link to="/contact" onClick={this.togglechange} >Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Navbar/>
-        </Content>
-    </Layout>
-    </div>
-      </div>
-
-</BrowserRouter>  
+          <ParticlesContainer />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <Layout>
+              <Header
+                className="header-color"
+                title={
+                  <Link className="header-link" to="/">
+                    {" "}
+                    Nagi Reddy
+                  </Link>
+                }
+                scroll
+              >
+                <Navigation>
+                  <Link to="/about" onClick={this.addActiveClass}>
+                    About
+                  </Link>
+                  <Link to="/projects" onClick={this.addActiveClass}>
+                    Projects
+                  </Link>
+                  <Link to="/cv" onClick={this.addActiveClass}>
+                    CV
+                  </Link>
+                  <Link to="/contact" onClick={this.addActiveClass}>
+                    Contact
+                  </Link>
+                </Navigation>
+              </Header>
+              <Drawer
+                title={
+                  <Link className="drawer-link" to="/">
+                    Nagi Reddy
+                  </Link>
+                }
+              >
+                <Navigation>
+                  <Link to="/about" onClick={this.togglechange}>
+                    About
+                  </Link>
+                  <Link to="/projects" onClick={this.togglechange}>
+                    Projects
+                  </Link>
+                  <Link to="/cv" onClick={this.togglechange}>
+                    CV
+                  </Link>
+                  <Link to="/contact" onClick={this.togglechange}>
+                    Contact
+                  </Link>
+                </Navigation>
+              </Drawer>
+              <Content>
+                <div className="page-content" />
+                <Navbar />
+              </Content>
+            </Layout>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Components/Pages/contacts.js
+++ src/Components/Pages/contacts.js
@@ -1,40 +1,84 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-import { List, ListItem, ListItemContent } from 'react-mdl';
-import { BouncyDivRight } from '../Animations/animation';
-import MyDetails from '../Reusable/NameDetails';
-
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import { List, ListItem, ListItemContent } from "react-mdl";
+import { BouncyDivRight } from "../Animations/animation";
+import MyDetails from "../Reusable/NameDetails";
 
 class About extends Component {
   render() {
     return (
       <div className="contact-container container">
         <div className="main-name-container">
-          <Grid className="name-container" >
+          <Grid className="name-container">
             <MyDetails />
             <Cell className="name__container" col={6}>
-              <BouncyDivRight> <h2 className="contact-details">Contact Datails </h2>
+              <BouncyDivRight>
+                {" "}
+                <h2 className="contact-details">Contact Datails </h2>
                 <List>
                   <ListItem>
-                    <ListItemContent className="ListItemContent"> <a href="tel:+372-53950665" target="_blank" rel="noopener noreferrer"> <i className="fa fa-phone-square" ></i>(372) 53950665 </a> </ListItemContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemContent className="ListItemContent" ><a href="mailto:nagireddy.panditi@gmail.com" rel="noopener noreferrer" target="_blank"  > <i className="fa fa-envelope" id="special-item"></i>Nagireddy.panditi@gmail.com</a></ListItemContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemContent className="ListItemContent"><a href="https://wa.me/37253950665" target="_blank" rel="noopener noreferrer"> <i className="fa fa-whatsapp"></i>(372) 53950665 </a></ListItemContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemContent className="ListItemContent"><a href="https://www.facebook.com/alwayssmile1234" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-square"></i> Naga Reddy</a>
+                    <ListItemContent className="ListItemContent">
+                      {" "}
+                      <a
+                        href="tel:+372-53950665"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {" "}
+                        <i className="fa fa-phone-square"></i>(372) 53950665{" "}
+                      </a>{" "}
                     </ListItemContent>
                   </ListItem>
                   <ListItem>
-                    <ListItemContent className="ListItemContent"> <a href="skype:nagireddy123451?call"> <i className="fa fa-skype" ></i>Nagireddy123451</a></ListItemContent>
+                    <ListItemContent className="ListItemContent">
+                      <a
+                        href="mailto:nagireddy.panditi@gmail.com"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="fa fa-envelope" id="special-item"></i>
+                        Nagireddy.panditi@gmail.com
+                      </a>
+                    </ListItemContent>
                   </ListItem>
-                </List> 
+                  <ListItem>
+                    <ListItemContent className="ListItemContent">
+                      <a
+                        href="https://wa.me/37253950665"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {" "}
+                        <i className="fa fa-whatsapp"></i>(372) 53950665{" "}
+                      </a>
+                    </ListItemContent>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemContent className="ListItemContent">
+                      <a
+                        href="https://www.facebook.com/alwayssmile1234"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {" "}
+                        <i className="fa fa-facebook-square"></i> Naga Reddy
+                      </a>
+                    </ListItemContent>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemContent className="ListItemContent">
+                      {" "}
+                      <a href="skype:nagireddy123451?call">
+                        {" "}
+                        <i className="fa fa-skype"></i>Nagireddy123451
+                      </a>
+                    </ListItemContent>
+                  </ListItem>
+                </List>
               </BouncyDivRight>
             </Cell>
-          </Grid>    
+          </Grid>
         </div>
       </div>
     );
