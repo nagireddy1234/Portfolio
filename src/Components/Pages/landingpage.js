37c5ff12ac8f568168d5046d06e35ffe6741b6cd
nagireddy1234 <nagireddy.panditi@gmail.com>
2021-12-20 08:53:22 +0200

    A small change

--- src/Components/Pages/landingpage.js
+++ src/Components/Pages/landingpage.js
@@ -10,18 +10,17 @@ class Landing extends Component {
       <div className="landing-grid-container container">
         <Grid className="landing-grid">
           <BouncyDiv>
-            {" "}
             <Cell col={12}>
               <img src={Image} className="avatar-img" alt="avatar"></img>
               <div className="Banner-text">
                 <Typing hideCursor>
                   <Typing.Speed ms={75} />
-                  <h1>Full stack web developer</h1>{" "}
+                  <h1>Full stack web developer</h1>
                 </Typing>
                 <p>
-                  JavaScript | TypeScript | Jquery | React Hooks | Redux |
+                  JavaScript | TypeScript | React js | React Hooks | Redux |
                   GitHub/Git | Node Js | Mongo DB | Vue | HTML5/CSS3/Bootstrap |
-                  UX design
+                  Jquery | Figma
                 </p>
                 <div className="social-icons">
                   <a
@@ -52,6 +51,7 @@ class Landing extends Component {
                   >
                     <i class="fa fa-stack-overflow"></i>
                   </a>
+
                   <a
                     href="mailto:nagireddy.panditi@gmail.com"
                     rel="noopener noreferrer"
@@ -61,7 +61,7 @@ class Landing extends Component {
                   </a>
                 </div>
               </div>
-            </Cell>{" "}
+            </Cell>
           </BouncyDiv>
         </Grid>
       </div>
